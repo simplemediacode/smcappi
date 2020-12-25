e2cfa6054dc5e0742ca07b081420f0701318d6ef
rolandinsh <rolands@mediabox.lv>
2020-12-26 00:31:57 +0200

    rm test methods

--- src/SmcAppi.php
+++ src/SmcAppi.php
@@ -12,22 +12,9 @@ class SmcAppi
     public function __construct()
     {
         $this->version = $this->version();
-        $this->configs = $this->loadConfigs();
     }
     public function version()
     {
         return '1.0.0';
-    }
-
-    public function getConfig()
-    {
-        $configs = new Config();
-        return $configs->version;
-    }
-    
-    public function loadConfigs()
-    {
-        $config = new Config();
-        return $config->version;
     }
 }
