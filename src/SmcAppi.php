47253335f734f71710b21ef4a0ce8a690ae18d2a
rolandinsh <rolands@mediabox.lv>
2020-12-25 23:14:35 +0200

    test 1

--- src/SmcAppi.php
+++ src/SmcAppi.php
@@ -7,19 +7,27 @@ use SmcAppi\Configs\Config;
 class SmcAppi
 {
     var $version;
-    
+    var $configs;
+
     public function __construct()
     {
         $this->version = $this->version();
+        $this->configs = $this->loadConfigs();
     }
     public function version()
     {
         return '1.0.0';
     }
 
-    public function getConfig(){
+    public function getConfig()
+    {
         $configs = new Config();
         return $configs->version;
-        
+    }
+    
+    public function loadConfigs()
+    {
+        $config = new Config();
+        return $config->version;
     }
 }
